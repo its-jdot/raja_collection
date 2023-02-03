72d1205e30fecc9e7e46cc1262efc657f253bede
Jamshad Arshad <j.arshad1285@gmail.com>
2023-02-03 17:32:22 +0500

    Update Home Page

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,12 @@
 class Customer < ApplicationRecord
+
+
+    def created_at
+        attributes['created_at'].strftime("%a,%e %b, %Y, %H:%M")
+    end
+
+    def updated_at
+        attributes['updated_at'].strftime("%a,%e %b, %Y, %H:%M")
+    end
+
 end
